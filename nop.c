a0f9f35c1c803f548040a44e89963b8da27dc427
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-14 14:12:27 +0000

    Implementing nop.c

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - This does not do anything
+ * @stack: double pointer referring to start of stack
+ * @line_number: line number
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
